5546c161c8853c68ae4466d4df1e0f0b9f4dfc50
Ubuntu <ubuntu@ip-172-31-17-225.ap-northeast-1.compute.internal>
2025-09-14 11:57:52 +0000

    [Finish] アプリケーションに必要なファイルの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/bookers' => 'homes#top' 
+
+  scope '/bookers' do
+    resources :books
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
